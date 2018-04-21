fc42b7c745e8af0e5be85c157283438aa086c84b
wtfenfenmiao <wtbupt@outlook.com>
2018-04-21 22:13:49 +0800

    抽完了时间戳特征

--- pingan/data_process.py
+++ pingan/data_process.py
@@ -27,13 +27,35 @@ import pandas as pd
 
 """
 
-print ('Load data......')
-train_data=pd.read_csv('PINGAN-2018-train_demo.csv')
-print (train_data)
 
+if __name__=='__main__':
+    print ('Load data......')
+    train_data=pd.read_csv('PINGAN-2018-train_demo.csv')
+
+    """时间特征处理，抽出了星期，月份，小时，但是还没有做哑编码（one-hot）!!!!!!!"""
+    #把数据加上星期，月份，小时这三个空列，后面再补内容
+    train_columns = list(train_data.columns)  # 获取列名
+    train_columns.append("weekday")
+    train_columns.append("month")
+    train_columns.append("hour")
+    train_data.reindex(columns=train_columns)  # 为了添加星期，月份，小时这三个特征
+    print(train_columns)
+
+    #时间处理
+    # Unix时间戳，1476923580这样子的，转成2016-10-20 08:33:00这样子的
+    train_data["TIME"]=pd.to_datetime(train_data["TIME"],unit='s')   #把unix时间转成人能看明白的，单位到秒（s）
+    #这个dt太方便了，要啥取啥就行
+    train_data["weekday"]=train_data["TIME"].dt.weekday  #星期，The day of the week with Monday=0, Sunday=6
+    train_data["month"] = train_data["TIME"].dt.month
+    train_data["hour"] = train_data["TIME"].dt.hour
+
+    print (train_data)
+    # print (train_data["TIME"].dt.weekday)
+    # print (train_data["TIME"].dt.month)
+    # print (train_data["TIME"].dt.hour)
 #先做数据预处理，比如归一化，抽特征等等，整全了再划分train和validation
 #第一列用户ID不要了
-#第二列time弄出来年月日星期小时，感觉星期和小时应该是比较关键的特征
+#第二列time弄出来年月日星期小时，感觉月，星期，和小时应该是比较关键的特征
 #行程id没想好，感觉不一定要用啊，可能跟用户ID一样？
 #经纬度不能归一化或者标准化，就这么放着，不能动，归一化啥的这个信息就丢了
 #方向大概得标准化/归一化，还要把负值处理一下，负值是无效信息，算是“缺失值”，要做缺失值处理
