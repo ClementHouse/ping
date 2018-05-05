bab857539b3898b667d88a45f7cdbada642df191
wtfenfenmiao <wtbupt@outlook.com>
2018-05-05 12:09:59 +0800

    把电话状态这个特征删了，发现就变差了，看样子这个还得留着......看官方baseline吧

--- pingan/test.py
+++ pingan/test.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
-data=pd.DataFrame({'A':[1,1,2,2],'B':['a','b','a','b']})
-print ()
-print (data)
 
-data=data.reindex(columns=['A','B','C'])
-data['C']=np.array(data['A'])
-print (data)
+
+
+if __name__=="__main__":
+    # data=pd.DataFrame({'A':[1,1,2,2],'B':['a','b','a','b']})
+    # print ()
+    # print (data)
+    #
+    # data=data.reindex(columns=['A','B','C'])
+    # data['C']=np.array(data['A'])
+    # print (data)
